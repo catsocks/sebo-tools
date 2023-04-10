95b5abbb37749aba959518637ed477832cd21ad8
catsocks <3389801+catsocks@users.noreply.github.com>
2023-04-10 19:51:47 -0300

    Fix `test_rename` and `test_create_cover`

--- tests/test_sebo_wand.py
+++ tests/test_sebo_wand.py
@@ -26,8 +26,8 @@ def test_rename(data_path):
     assert (data_path / "book-a" / "2.jpg").exists()
     assert (data_path / "book-a" / "book-d" / "2.jpg").exists()
     assert (data_path / "book-a" / "book-d" / "Cake recipe.txt").exists()
-    assert (data_path / "book-b" / "2.jpg").exists()
-    assert (data_path / "book-b" / "3.jpeg").exists()
+    assert (data_path / "book-b" / "2.jpeg").exists()
+    assert (data_path / "book-b" / "3.jpg").exists()
     assert (data_path / "book-b" / "4.heic").exists()
 
 
