db36feac4b041b137b490f6d0f056f4486b1424d
catsocks <3389801+catsocks@users.noreply.github.com>
2023-04-15 12:18:00 -0300

    Add `test_covergen_using_remove_bg`

--- tests/test_covergen.py
+++ tests/test_covergen.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from click.testing import CliRunner
 
@@ -5,7 +6,7 @@ from click.testing import CliRunner
 from sebo_tools.covergen import cli
 
 
-def test_covergen_with_rembg(data_path):
+def test_covergen_using_rembg(data_path):
     pytest.importorskip("rembg")
 
     runner = CliRunner()
@@ -13,3 +14,17 @@ def test_covergen_with_rembg(data_path):
     result = runner.invoke(cli, ["--use-rembg", str(book_path)])
     assert result.exit_code == 0
     assert (book_path / "1.png").exists()
+
+
+def test_covergen_using_remove_bg(data_path):
+    envvar = "REMOVE_BG_API_KEY"
+    if os.getenv(envvar) is None:
+        pytest.skip(f"Please set the environment variable {envvar} to test the "
+            + "remove.bg API.")
+
+    runner = CliRunner()
+    book_path = data_path / "book-b"
+    result = runner.invoke(cli, ["--use-remove-bg", str(book_path)])
+    assert result.exit_code == 0
+    assert (book_path / "1.png").exists()
+
